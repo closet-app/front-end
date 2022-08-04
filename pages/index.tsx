521cb670c8034f6b599425255996c4491d748f64
ChrisJRamirez <chrisjamesramirez@gmail.com>
2022-08-04 13:43:11 -0400

    initalized typescript nextjs with chakraui

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Container,Text } from '@chakra-ui/react';
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      <Text>I am cool looking text</Text>
+    </Container>
+  )
+}
+
+export default Home
