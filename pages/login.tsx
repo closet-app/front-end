7363fa1110a9e58a7cd9649c2bf11f0852deccb4
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-07 00:00:40 -0400

    finished creating login component

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,32 +1,33 @@
 import {
-  Box,
-  Container,
   Flex,
-  Button,
+  Container,
   Heading,
-  Text,
+  Button,
+  Box,
   Link,
+  Text,
 } from "@chakra-ui/react";
-import { Form, Formik } from "formik";
+import { Formik, Form } from "formik";
 import InputField from "../components/InputField";
 import Layout from "../components/layouts/Article";
 import NextLink from "next/link";
-const Register = () => {
+
+interface LoginProps {}
+
+const Login = ({}: LoginProps) => {
   return (
-    <Layout title="Register" base={true}>
+    <Layout title="Login" base={true}>
       <Flex height="full" justifyContent="center" alignItems="center">
-        <Container maxW={{ base: "xs", sm: "sm", md: "xl" }}>
+        <Container maxW={{ base: "xs", sm: "sm", md: "lg" }}>
           <Box margin={["1rem", "2rem"]}>
             {/* put logo above or below heading */}
             <Heading as="h2" textAlign="center">
-              Welcome to Closet
+              Closet
             </Heading>
           </Box>
           <Box padding="2rem" boxShadow="dark-lg" rounded="xl">
             <Formik
               initialValues={{
-                firstName: "",
-                lastName: "",
                 email: "",
                 password: "",
               }}
@@ -36,24 +37,10 @@ const Register = () => {
             >
               {({ isSubmitting }) => (
                 <Form>
-                  <Text mb={4} textAlign="center" fontSize="2xl">
-                    Sign up for a free account
-                  </Text>
-                  <Box
-                    width="full"
-                    display={{ md: "flex" }}
-                    justifyContent="space-between"
-                    alignItems="center"
-                  >
-                    <Box>
-                      <InputField name="firstName" label="First Name" />
-                    </Box>
-                    <Box>
-                      <InputField name="lastName" label="First Name" />
-                    </Box>
-                  </Box>
-
                   <Box mt={4}>
+                    <Text mb={4} textAlign="center" fontSize="2xl">
+                      Welcome Back
+                    </Text>
                     <InputField name="email" label="Email" type="email" />
                   </Box>
 
@@ -77,23 +64,26 @@ const Register = () => {
                       colorScheme="telegram"
                       width="50%"
                     >
-                      Join
+                      Log in to your account
                     </Button>
                   </Box>
                 </Form>
               )}
             </Formik>
 
-            <Box
+            <Flex
               mt={4}
-              display="flex"
-              justifyContent="center"
+              width="full"
+              justifyContent="space-between"
               alignItems="center"
             >
-              <NextLink href="/login" passHref>
-                <Link color="#0088cc">Already have an account?</Link>
+              <NextLink href="/register" passHref>
+                <Link color="#0088cc">Don't have an account?</Link>
               </NextLink>
-            </Box>
+              <NextLink href="/password-reset" passHref>
+                <Link color="#0088cc">Forgot password?</Link>
+              </NextLink>
+            </Flex>
           </Box>
         </Container>
       </Flex>
@@ -101,4 +91,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
