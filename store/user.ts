662b78723faa13c0b6977561d110e83270ab82e9
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-13 21:42:59 -0400

    implemented login and register to use zustand

---
+++ store/user.ts
@@ -0,0 +1,35 @@
+import create from "zustand";
+import { devtools, persist } from "zustand/middleware";
+
+interface UserState {
+  id: number | undefined;
+  firstName: string;
+  lastName: string;
+  email: string;
+  token: string;
+  login: (options: {
+    id: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+    token: string;
+  }) => void;
+}
+
+const useUserStore = create<UserState>()(
+  devtools(
+    persist((set) => ({
+      id: undefined,
+      firstName: "",
+      lastName: "",
+      email: "",
+      token: "",
+      login: ({ id, email, firstName, lastName, token }) => {
+        set({ id, email, firstName, lastName, token });
+        localStorage.setItem("token", token);
+      },
+    }))
+  )
+);
+
+export default useUserStore;
